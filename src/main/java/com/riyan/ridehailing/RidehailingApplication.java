76e89c6d747a0fba7adfe78b6ef7177d16bc8f0b
zm24acgRiyan <zm24acg@herts.ac.uk>
2025-05-27 05:34:46 +0530

    Initial commit with Spring Boot

---
+++ src/main/java/com/riyan/ridehailing/RidehailingApplication.java
@@ -0,0 +1,13 @@
+package com.riyan.ridehailing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidehailingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidehailingApplication.class, args);
+	}
+
+}
